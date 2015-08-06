d34218d6561f54f3594fc3b2ec9dcef00eae52c0
Downvector <downvector@gmail.com>
2015-08-06 08:41:22 +0200

    add mindmap logC

--- index_mindmap-freemind.mm
+++ index_mindmap-freemind.mm
@@ -1,12 +1,18 @@
-<map version="0.9.0">
+<map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
 <node CREATED="1200684332203" ID="ID_586662378" MODIFIED="1438527351398" STYLE="fork" TEXT="Index.html">
 <node CREATED="1438527143894" ID="ID_1685286576" MODIFIED="1438527311666" POSITION="right" TEXT="Selected projects"/>
 <node CREATED="1438527163566" ID="ID_935762393" MODIFIED="1438527311666" POSITION="right" TEXT="Experience"/>
 <node CREATED="1438527167469" ID="ID_1640051704" MODIFIED="1438527311666" POSITION="right" TEXT="Role inside teams"/>
 <node CREATED="1438527207942" ID="ID_957727440" MODIFIED="1438527324892" POSITION="right" TEXT="Tasks breakdown">
-<node CREATED="1438527218833" ID="ID_551726374" MODIFIED="1438527311666" TEXT="Tasks 100%">
+<node CREATED="1438527218833" ID="ID_551726374" MODIFIED="1438839814318" TEXT="Tasks 100%">
 <node CREATED="1438527371390" ID="ID_1128731938" MODIFIED="1438527385828" TEXT="owning UI design tasks, taking them from game designers"/>
+<node CREATED="1438839830916" ID="ID_228044033" MODIFIED="1438839839920" TEXT="live UI prototype on the page">
+<node CREATED="1438840104552" ID="ID_1674780094" MODIFIED="1438840120775" TEXT="66/33 layout"/>
+<node CREATED="1438840122128" ID="ID_1657108219" MODIFIED="1438840175637" TEXT="&quot;click inside the frame to start the prototype. You need: this or that browser, and a xbox controller"/>
+<node CREATED="1438840183376" ID="ID_93032154" MODIFIED="1438840192189" TEXT="detects all controllers currently connected"/>
+</node>
+<node CREATED="1438839857994" ID="ID_1939425527" MODIFIED="1438839877783" TEXT="a visualization of user-testing work. Chart or photo or ?"/>
 </node>
 <node CREATED="1438527225761" ID="ID_1385195679" MODIFIED="1438527311666" TEXT="Tasks 50%"/>
 <node CREATED="1438527228662" ID="ID_584645095" MODIFIED="1438527311666" TEXT="Tasks 0%"/>
