301dac155f94c9099e5ad60d79c0d64cc31782bf
Downvector <downvector@gmail.com>
2015-08-24 08:24:05 +0200

    wordsmithing mindmap

--- index_mindmap-freemind.mm
+++ index_mindmap-freemind.mm
@@ -17,22 +17,32 @@
 <node CREATED="1438527225761" ID="ID_1385195679" MODIFIED="1438527311666" TEXT="Tasks 50%"/>
 <node CREATED="1438527228662" ID="ID_584645095" MODIFIED="1438527311666" TEXT="Tasks 0%"/>
 </node>
-<node CREATED="1440228103870" ID="ID_378152151" MODIFIED="1440229913901" POSITION="left" TEXT="Interview">
-<node CREATED="1440229888793" HGAP="21" ID="ID_1420883577" MODIFIED="1440246147719" TEXT="Interaction-design for other designers&apos; features" VSHIFT="-72">
+<node CREATED="1440228103870" ID="ID_378152151" MODIFIED="1440394883019" POSITION="left" TEXT="Interview">
+<node CREATED="1440229913902" HGAP="23" ID="ID_1664120479" MODIFIED="1440351589017" TEXT="Interaction design (cross-platforms): the prototype for trackmania map editor" VSHIFT="35">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1440254090222" ID="ID_1661722090" MODIFIED="1440254108971" TEXT="Learn the team&apos;s work style"/>
+<node CREATED="1440254109510" ID="ID_684415761" MODIFIED="1440254133899" TEXT="Early, bet all on feedback, feedback to every user action. Only then start working on tutorials"/>
+<node CREATED="1440229888793" HGAP="21" ID="ID_1420883577" MODIFIED="1440334304644" TEXT="Interaction-design for other designers&apos; features" VSHIFT="-72">
+<font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1440229732555" ID="ID_722653357" MODIFIED="1440229758799" TEXT="Website design: player community engagement">
 <node CREATED="1440230532420" HGAP="146" ID="ID_667458710" MODIFIED="1440246086624" TEXT="We wanted to push the groups and vanity aspects, you have a &quot;tutorial completed&apos; counter for modding UT" VSHIFT="-107"/>
 </node>
 </node>
-<node CREATED="1440229913902" HGAP="23" ID="ID_1664120479" MODIFIED="1440246145383" TEXT="Feature design -inventing stuff" VSHIFT="35">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1440254090222" ID="ID_1661722090" MODIFIED="1440254108971" TEXT="Learn the team&apos;s work style"/>
-<node CREATED="1440254109510" ID="ID_684415761" MODIFIED="1440254133899" TEXT="Early, bet all on feedback, feedback to every user action. Only then start working on tutorials"/>
+<node CREATED="1440334307370" ID="ID_1197742557" MODIFIED="1440335503651" TEXT="conceptual design" VSHIFT="-74"/>
+<node CREATED="1440334316866" ID="ID_35739296" MODIFIED="1440335505779" TEXT="wireframes" VSHIFT="-80"/>
+<node CREATED="1440334319706" HGAP="17" ID="ID_603952691" MODIFIED="1440335509394" TEXT="walkthroughs and scenarios" VSHIFT="-44"/>
+<node CREATED="1440335207828" ID="ID_1886612091" MODIFIED="1440335211761" TEXT="competitors analysis">
+<node CREATED="1440335213100" ID="ID_706543118" MODIFIED="1440335308368" TEXT="splatoon: improved the least interesting steps in the user flow for online shooters: moving, from running the game to the servers list, waiting for match to fill-up (note: they don&apos;t show you disconnections to avoid influencing your behavior)"/>
+<node CREATED="1440335313435" ID="ID_430948728" MODIFIED="1440335351504" TEXT="Heroes of the storm: tab-score screen has enough categories that you fille like you&apos;re among the top3 in one category (frog-pond effect)"/>
 </node>
-<node CREATED="1440230924546" HGAP="43" ID="ID_586114576" MODIFIED="1440246516004" TEXT="User-testing" VGAP="6" VSHIFT="99">
+<node CREATED="1440335470338" ID="ID_1299624112" MODIFIED="1440335499266" TEXT="personas" VSHIFT="90">
+<node CREATED="1440335473602" ID="ID_87067228" MODIFIED="1440335494503" TEXT="not used yet but the GDC talk from harmonix on using comics in design documents is great"/>
+</node>
+</node>
+<node CREATED="1440230924546" HGAP="43" ID="ID_586114576" MODIFIED="1440351612917" TEXT="User-testing: the feedback-logger method" VGAP="6" VSHIFT="99">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1440230934057" HGAP="27" ID="ID_1217476870" MODIFIED="1440249284657" TEXT="Test organization" VSHIFT="-54">
-<node CREATED="1440230943170" ID="ID_504325407" MODIFIED="1440231102841" TEXT="Proposing objectives">
+<node CREATED="1440230943170" ID="ID_504325407" MODIFIED="1440351509398" TEXT="Proposing objectives: the trackmania research strategy">
 <node CREATED="1440249238712" HGAP="18" ID="ID_781005666" MODIFIED="1440255327157" TEXT="Epic Games&apos; projects" VSHIFT="-37">
 <node CREATED="1440231102842" HGAP="21" ID="ID_1080966022" MODIFIED="1440246099295" TEXT="Epic Launcher: no info on disk size required to install" VSHIFT="32"/>
 <node CREATED="1440230952913" HGAP="23" ID="ID_893579660" MODIFIED="1440246097159" TEXT="Fortnite: do players realize they can destroy their own constructions (ex: stairs during the livestream)" VSHIFT="-34"/>
@@ -52,7 +62,9 @@
 </node>
 <node CREATED="1440256760427" ID="ID_1133292884" MODIFIED="1440256774879" TEXT="Meeting expectations (set by Marketing)"/>
 </node>
-<node CREATED="1440253423978" HGAP="19" ID="ID_1313019437" MODIFIED="1440253449659" TEXT="Treating data" VSHIFT="42"/>
+<node CREATED="1440253423978" HGAP="19" ID="ID_1313019437" MODIFIED="1440253449659" TEXT="Treating data" VSHIFT="42">
+<node CREATED="1440351341402" ID="ID_1500780891" MODIFIED="1440351381166" TEXT="I developed the feedback-logger method, because: long treatment time causes problem, not in sync with the dev team&apos;s current focus"/>
+</node>
 <node CREATED="1440253430067" HGAP="19" ID="ID_934292095" MODIFIED="1440253446380" TEXT="Presenting results" VSHIFT="109">
 <node CREATED="1440257741476" ID="ID_634008945" MODIFIED="1440257779016" TEXT="Not just another opinion, but instead say those results come from scientific approach (unemotional, I am emotionally detached from my results)"/>
 <node CREATED="1440257794444" ID="ID_67253988" MODIFIED="1440257819849" TEXT="Not trying to override the game designers&apos; work, just suggestion based on things like Gestalt law"/>
@@ -61,7 +73,7 @@
 <node CREATED="1440228130717" HGAP="46" ID="ID_1764005967" MODIFIED="1440249290640" TEXT="Statistical analysis (n=16)" VSHIFT="-17">
 <node CREATED="1440229852802" ID="ID_1265888839" MODIFIED="1440229878174" TEXT="Analyzing reviews+forum comments"/>
 </node>
-<node CREATED="1440246126102" HGAP="22" ID="ID_1675065269" MODIFIED="1440246289205" TEXT="Academic Theory" VSHIFT="80">
+<node CREATED="1440246126102" HGAP="22" ID="ID_1675065269" MODIFIED="1440335120604" TEXT="Academic Theory" VSHIFT="80">
 <font NAME="SansSerif" SIZE="12"/>
 <node CREATED="1440246264653" ID="ID_235143846" MODIFIED="1440246270898" TEXT="Cognitive Psychology">
 <node CREATED="1440246826793" ID="ID_1629061540" MODIFIED="1440246839171" TEXT="I use this pdf">
@@ -82,6 +94,40 @@
 <node CREATED="1440246587828" HGAP="18" ID="ID_1441296295" MODIFIED="1440257832052" TEXT="Being embedded within dev teams" VSHIFT="-21"/>
 <node CREATED="1440246730362" ID="ID_1397313266" MODIFIED="1440257841052" TEXT="We do not make games easier: example of " VSHIFT="16"/>
 <node CREATED="1440257648661" HGAP="22" ID="ID_1512458600" MODIFIED="1440257839428" TEXT="Building trust before trying to impose your process: first explain that together we are going to save time and money" VSHIFT="28"/>
+</node>
+<node CREATED="1440333587759" ID="ID_1336475602" MODIFIED="1440351330924" TEXT="Career: what I want to do" VSHIFT="134">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1440333602231" ID="ID_416177497" MODIFIED="1440350760724" TEXT="living in the US" VSHIFT="-31"/>
+<node CREATED="1440333605503" ID="ID_684947092" MODIFIED="1440350550013" TEXT="work on different types of projects: games, game engines, websites" VSHIFT="-4"/>
+<node CREATED="1440333970581" HGAP="25" ID="ID_1957624844" MODIFIED="1440334443354" TEXT="UX is an umbrella term, I identify myself as an interaction-designer and a user-researcher" VSHIFT="28"/>
+<node CREATED="1440334428753" ID="ID_1973964577" MODIFIED="1440350582525" TEXT="being a specialist in a big company/publisher: being expert at something on project with very tight schedule, finally finding a solution for the team" VSHIFT="21"/>
+<node CREATED="1440335128612" ID="ID_1709162657" MODIFIED="1440335397872" TEXT="Not a senior: less good at generating new ideas but strong at implementing a detailed solution. I still want to work under someone&apos;s direction for a while. I knew at Ankama that their offer came too early"/>
+<node CREATED="1440349938019" HGAP="23" ID="ID_1839665431" MODIFIED="1440350174939" TEXT="Ankama: wanted to work on multiple projects at once, embedded within teams" VSHIFT="27">
+<node CREATED="1440350143995" ID="ID_187187927" MODIFIED="1440350168975" TEXT="I was recruited after making my portfolio webside dedicated to them and the position, how I could fit"/>
+<node CREATED="1440350039780" ID="ID_1102452735" MODIFIED="1440350063464" TEXT="I left because I felt not competent enough for the job offered"/>
+</node>
+<node CREATED="1440349961964" ID="ID_1675877820" MODIFIED="1440349979090" TEXT="Crytek: wanted to work on both games and engine, both design and user-testing">
+<node CREATED="1440350188386" ID="ID_853617840" MODIFIED="1440350331446" TEXT="I was noticed through twitter by the lead UX designer and offered a job "/>
+<node CREATED="1440350033859" ID="ID_828594402" MODIFIED="1440350090152" TEXT="I left because the project got killed and they told me that no team work would come for the next 6 months"/>
+</node>
+<node CREATED="1440349980134" ID="ID_199085622" MODIFIED="1440350009265" TEXT="Ubisoft: wanted to see how my skills compared with some of the best in the field">
+<node CREATED="1440350345449" ID="ID_283777851" MODIFIED="1440350426374" TEXT="I asked a friend whom I worked with at school, he told his lead that I could fit the role"/>
+<node CREATED="1440350094027" ID="ID_497009806" MODIFIED="1440350126256" TEXT="I have been away from development teams for too long. And the HQ structure does not allow me to help dev teams enough"/>
+</node>
+</node>
+<node CREATED="1440338671884" ID="ID_1632007867" MODIFIED="1440351332578" TEXT="My questions" VSHIFT="168">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1440338679259" ID="ID_1778715753" MODIFIED="1440395592282" TEXT="is it a new, or existing position? if new, are the responsabilities new or taken from other people?">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1440351252379" ID="ID_1995327552" MODIFIED="1440395628151" TEXT="What are epic&apos;s plans regarding the UX team? Will it grow by several employees soon?">
+<icon BUILTIN="info"/>
+</node>
+<node CREATED="1440338891954" ID="ID_1645035914" MODIFIED="1440338904558" TEXT="Where in the floorplan is located the UX team? all in the same place?"/>
+<node CREATED="1440351292772" ID="ID_282268635" MODIFIED="1440351304247" TEXT="What do you expect me to accomplish in the first 60 to 90 days?"/>
+<node CREATED="1440338795754" ID="ID_1088316197" MODIFIED="1440338828255" TEXT="who would be my manager, what is his management style? (frequent feedback vs no feedback)"/>
+<node CREATED="1440338861314" ID="ID_1477405636" MODIFIED="1440338880350" TEXT="What type of persons tend to succeed in this company? What type fail?"/>
 </node>
 </node>
 </node>
